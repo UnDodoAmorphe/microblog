a06ea66e8d1eb0151ec5034a96ec223c381e873f
Florian Gautier <flo.gautier10@gmail.com>
2019-10-23 16:11:30 +0200

    Frontend

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,42 @@
+<template>
+<section id="threadList">
+    <label>
+        NOM DU CHANNEL
+    </label>
+    <button class="btn btn-lg btn-block" @click="isEnabled = !isEnabled">
+        Commencer un nouveau thread
+    </button>
+    <input type="text" v-show="isEnabled" placeholder="Choisissez un titre">
+
+<ul id="list" class="list-group">
+    <li v-for="item in items" v-bind:key="item.id" @click="content" class="list-group-item">
+        {{item.title}}
+    </li>
+</ul>
+</section>
+</template>
+
+<script>
+    export default {
+        name: "ThreadList",
+        data () {
+            return {
+                items: [
+                    {title: "Foo",
+                     body: "Lorem ipsum etc"},
+                    {title: "Bar",
+                     body: "Ipsum lorem etc"}
+                ],
+                isEnabled: false
+            }
+        },
+        components: {
+        }
+    }
+</script>
+
+<style scoped>
+    li {
+        margin-top: 5px;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="home container">
+   <nav>
+       <a href="">NAV 1</a>
+       <a href="">NAV 2</a>
+   </nav>
+    <div class="row">
+        <section class="col-lg-2">Thèmes</section>
+        <ThreadList class="col-lg-4"></ThreadList>
+        <section class="col-lg-6">Commentaires</section>
+    </div>
+    <div class="row">
+        <footer>
+            <p>Je suis le footer</p>
+        </footer>
+    </div>
+</div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import ThreadList from "@/components/ThreadList.vue"
+
+    export default {
+        name: 'home',
+        components: {
+            ThreadList
+        }
+    }
+</script>
+
+
+<style scoped>
+    footer {
+    }
+    
+</style>
